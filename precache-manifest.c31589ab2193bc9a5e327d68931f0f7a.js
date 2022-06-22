814f7ed94aae5f82edb1a8a0cb4a3e23b32506d2
Gabriel <jimenezg905@gmail.com>
2022-06-22 17:08:13 -0400

    Updates

---
+++ precache-manifest.c31589ab2193bc9a5e327d68931f0f7a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b757ad4858a474c2541",
+    "url": "/Tenzies/static/css/main.be347a54.chunk.css"
+  },
+  {
+    "revision": "2b757ad4858a474c2541",
+    "url": "/Tenzies/static/js/main.2b757ad4.chunk.js"
+  },
+  {
+    "revision": "c7f81061443d47d030c9",
+    "url": "/Tenzies/static/js/1.c7f81061.chunk.js"
+  },
+  {
+    "revision": "3acee836ea9f9c6bc2bb",
+    "url": "/Tenzies/static/js/runtime~main.3acee836.js"
+  },
+  {
+    "revision": "a8b8f3667208db11c64d0b18ff5a45bf",
+    "url": "/Tenzies/static/media/dice-six-faces-one.a8b8f366.png"
+  },
+  {
+    "revision": "46fb2d07261cc421311c728433b11f59",
+    "url": "/Tenzies/static/media/dice-six-faces-two.46fb2d07.png"
+  },
+  {
+    "revision": "69bb5b46a8bb40dfad5851560a6f4667",
+    "url": "/Tenzies/static/media/dice-six-faces-three.69bb5b46.png"
+  },
+  {
+    "revision": "100fd7c1d8ce36fb72c7645e68114958",
+    "url": "/Tenzies/static/media/dice-six-faces-four.100fd7c1.png"
+  },
+  {
+    "revision": "8bd5f441791d23bf0cc760d705b7177b",
+    "url": "/Tenzies/static/media/dice-six-faces-five.8bd5f441.png"
+  },
+  {
+    "revision": "b7feddd790cdee0c09071e0737b05415",
+    "url": "/Tenzies/static/media/dice-six-faces-six.b7feddd7.png"
+  },
+  {
+    "revision": "2978bff7438310d7c86f6f1960b96782",
+    "url": "/Tenzies/index.html"
+  }
+];
